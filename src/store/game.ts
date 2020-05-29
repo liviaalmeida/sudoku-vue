c2ef35b7aae7cd5056ac5e263d55d9c7f2700595
liviaalmeida <leave.ah@gmail.com>
2020-05-29 17:56:29 -0300

    Activity #5

--- src/store/game.ts
+++ src/store/game.ts
@@ -1,11 +1,13 @@
 import { Module } from 'vuex'
+import { RootState } from './index'
 import { Board } from '@/domain/Board'
+import { Sugoku } from '@/assets/api'
 
 interface GameState {
 	board: Board;
 }
 
-const GameStore: Module<GameState, any> = {
+const GameStore: Module<GameState, RootState> = {
 	state: {
 		board: new Board()
 	},
@@ -13,10 +15,25 @@ const GameStore: Module<GameState, any> = {
 		board: (state: GameState) => state.board
 	},
 	mutations: {
-
+		updateBoard(state, value: Board) {
+			state.board = value
+		},
+		updateSolution(state, value: Board) {
+			state.board.solution = value
+		}
 	},
 	actions: {
+		async newBoard(context) {
+			context.dispatch('startLoad')
 
+			const board = await Sugoku.newAsync()
+			const solution = await Sugoku.solveAsync(board)
+
+			context.commit('updateBoard', board)
+			context.commit('updateSolution', solution)
+
+			context.dispatch('stopLoad')
+		}
 	}
 }
 
